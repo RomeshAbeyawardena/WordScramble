f74c1b569315f8ee8ee01e513d7da476540d2a02
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-04-23 23:26:21 +0100

    Added front-end

---
+++ WordScramble.Api/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordScramble.Api.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        public HomeController(IMediator mediator, IMapper mapper) 
+            : base(mediator, mapper)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
